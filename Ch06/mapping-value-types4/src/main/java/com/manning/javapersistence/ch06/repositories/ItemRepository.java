13d059b32619568615927b1fbd46b29919f8eab0
Булат <bulat1987@gmail.com>
2024-12-04 23:17:09 +0500

    end Ch06

---
+++ Ch06/mapping-value-types4/src/main/java/com/manning/javapersistence/ch06/repositories/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.manning.javapersistence.ch06.repositories;
+
+import com.manning.javapersistence.ch06.model.Item;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item, Long> {
+    Iterable<Item> findByMetricWeight(double weight);
+}
